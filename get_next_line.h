0f505f50d70c88ec1f78c9530946f604708eb39a
Loena Nascimento <loenascimentosilva@gmail.com>
2025-09-08 12:10:20 -0300

    feat: add functions prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 12:41:26 by loda-sil          #+#    #+#             */
-/*   Updated: 2025/09/03 13:15:23 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/09/08 12:02:56 by loena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
